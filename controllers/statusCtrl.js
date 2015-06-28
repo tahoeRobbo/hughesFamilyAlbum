9a57b5c0e670df5bff8d7655c837e9d0c88c7b7b
tahoeRobbo <rob@snowyplayground.com>
2015-06-27 22:55:26 -0600

    added filter by family member, rm'd controller, service and views controller from the main - wrong - level of the app

--- controllers/statusCtrl.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('familyAlbum');
-
-app.controller('statusCtrl', function($scope, $location, $firebaseAuth, service) {
-	$scope.logout = function() {
-		service.logout();
-		$location.path('/login');
-	}
-})
